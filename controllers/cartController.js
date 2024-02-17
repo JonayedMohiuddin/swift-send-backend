228e114c5eff603e7f482f5f36a2ae27bec5082a
JonayedMohiuddin <jonayedmohiuddin@gmail.com>
2024-02-18 03:03:13 +0600

    UPDATED CART

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -6,14 +6,32 @@ async function index(req, res, next) {
 
     try {
         let query = `
-        SELECT * 
+        SELECT 
+        CI.ID AS CART_ITEM_ID,
+        CI.USER_ID,
+        CI.PRODUCT_ID,
+        CI.QUANTITY,
+
+        PR.ID AS PRODUCT_ID,
+        PR.SUPPLIER_ID,
+        PR.CATEGORY_ID,
+        PR.NAME AS PRODUCT_NAME,
+        PR.PRICE,
+        PR.IMAGE_URL,
+        PR.DISCOUNT,
+        PR.RATING,
+        PR.TOTAL_RATING,
+
+        S.NAME AS SUPPLIER_NAME
+
         FROM CART_ITEM CI
         JOIN PRODUCT PR ON CI.PRODUCT_ID = PR.ID
+        JOIN SUPPLIER S ON PR.SUPPLIER_ID = S.ID
         WHERE USER_ID = ${user.id}
+        ORDER BY S.NAME ASC, PR.NAME ASC
         `;
 
         let result = await databaseQuery(query);
-
         let cartItems = result.rows;
 
         return res.status(200).json({ cartItems: cartItems });
@@ -23,17 +41,65 @@ async function index(req, res, next) {
     }
 }
 
+//`INSERT INTO CART_ITEM (USER_ID, PRODUCT_ID, QUANTITY) VALUES (${user.id}, ${product_id}, ${quantity})`;
+
 async function cart_add_product_post(req, res, next) {
     let { product_id, quantity } = req.body;
     const user = req.user;
 
-    let query = `INSERT INTO CART_ITEM (USER_ID, PRODUCT_ID, QUANTITY) VALUES (${user.id}, ${product_id}, ${quantity})`;
-    let result = await databaseQuery(query);
+    try {
+        let query = `
+        BEGIN 
+            ADD_OR_UPDATE_CART_ITEM(${user.id}, ${product_id}, ${quantity}); 
+        END;`;
+        
+        let result = await databaseQuery(query);
 
-    res.send("Product added to cart.");
+        if(!result || result.rowsAffected === 0) {
+            throw new Error();
+        }
+
+        res.status(200).json({ successMessage: "Product added to cart successfully." });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ errorMessage: "Error in adding product to cart. Please try again." });
+    }
+}
+
+async function update_cart_item_quantity_post(req, res, next) {
+    let { cart_item_id, quantity } = req.body;
+
+    console.log("cart_item_id: " + cart_item_id);
+    console.log("quantity: " + quantity);
+
+    try {
+        let query = `UPDATE CART_ITEM SET QUANTITY = ${quantity} WHERE ID = ${cart_item_id}`;
+        let result = await databaseQuery(query);
+
+        res.status(200).json({ successMessage: "Cart item quantity updated successfully." });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ errorMessage: "Error in updating cart item quantity. Please try again." });
+    }
+}
+
+async function remove_cart_item_post(req, res, next) {
+    let { cart_item_id } = req.body;
+
+    try {
+        let query = `DELETE FROM CART_ITEM WHERE ID = ${cart_item_id}`;
+        let result = await databaseQuery(query);
+
+        res.status(200).json({ successMessage: "Cart item removed successfully." });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ errorMessage: "Error in removing cart item. Please try again." });
+    }
 }
 
 module.exports = {
     index,
     cart_add_product_post,
+    update_cart_item_quantity_post,
+    remove_cart_item_post,
 };
--- routes/cart.js
+++ routes/cart.js
@@ -11,6 +11,12 @@ router.get("/", cart_controller.index);
 // POST request to update Cart.
 router.post("/add/", cart_controller.cart_add_product_post);
 
+// POST request to update Cart.
+router.post("/update/", cart_controller.update_cart_item_quantity_post);
+
+// POST request to remove Cart item.
+router.post("/remove/", cart_controller.remove_cart_item_post);
+
 router.get("/test", (req, res, next) => {
     console.log("Cart test Successful");
     return res.json({ message: "Cart test Successful"});
